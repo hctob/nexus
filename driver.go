514511c08ed008776d6640203806856f4d8adb6d
Dan Bottcher <dbottch1@binghamton.edu>
2020-12-06 21:03:08 -0500

    Added basic driver code. Seems like it doesn't want to connect to the database even though it isn't displaying any errors.

---
+++ driver.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+    "fmt"
+    "flag"
+    "runtime"
+    "github.com/neo4j/neo4j-go-driver/neo4j"
+)
+
+/*
+*
+* Any structs/helper functions
+*/
+var (
+	arg_uri     = flag.String("uri", "bolt://localhost:7687", "The URI for the Nexus database, to connect to it.")
+    arg_username_raw     = flag.String("username", "dan", "Usernames are unique identifiers for database users.")
+    arg_password_raw     = flag.String("password", "test", "Unencrypted password for selected username.")
+
+    totalQueries int64
+)
+
+func helloWorld(uri, username, password string, encrypted bool) (string, error) {
+	driver, err := neo4j.NewDriver(uri, neo4j.BasicAuth(username, password, ""), func(c *neo4j.Config) {
+		c.Encrypted = encrypted
+	})
+	if err != nil {
+		return "", err
+	}
+	defer driver.Close()
+
+	session, err := driver.Session(neo4j.AccessModeWrite)
+	if err != nil {
+		return "", err
+	}
+	defer session.Close()
+
+	greeting, err := session.WriteTransaction(func(transaction neo4j.Transaction) (interface{}, error) {
+		result, err := transaction.Run(
+            "CREATE (n:Person { first_name: $fn, last_name: $ln }) RETURN n.first_name, n.last_name", map[string]interface{}{
+            "first_name":   "Quindarius",
+            "last_name": "Gooch",
+            })
+		if err != nil {
+			return nil, err
+		}
+
+		if result.Next() {
+			return result.Record().GetByIndex(0), nil
+		}
+
+		return nil, result.Err()
+	})
+	if err != nil {
+		return "", err
+	}
+
+	return greeting.(string), nil
+}
+
+/*
+result, err := session.Run("CREATE (n:Person { first_name: $fn, last_name: $ln }) RETURN n.first_name, n.last_name", map[string]interface{}{
+"first_name":   "Quindarius",
+"last_name": "Gooch",
+})
+*/
+
+/*
+* Main function for driver
+*/
+func main() {
+    runtime.GOMAXPROCS(256)
+	flag.Parse()
+
+    fmt.Println("Nexus Command Line Driver: ")
+    if *arg_username_raw == "" {
+        fmt.Println("god damn it\n")
+        return
+    }
+    if *arg_password_raw == "" {
+        fmt.Println("god damn it\n")
+        return
+    }
+
+    helloWorld("bolt://localhost:7687", *arg_password_raw, *arg_password_raw, false)
+    fmt.Println("Done")
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module main
+
+go 1.15
+
+require github.com/neo4j/neo4j-go-driver v1.8.3
